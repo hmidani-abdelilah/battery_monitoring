12462d76b9a16be570785a2aef1d1e46eeb25722
hmidani abdelilah <39446276+hmidani-abdelilah@users.noreply.github.com>
2025-12-14 16:09:20 +0100

    Update power.sh

--- power.sh
+++ power.sh
@@ -4,6 +4,9 @@ STATUSS=/sys/class/power_supply/BAT1/status
 STATUSS="${STATUSS//[$'\t\r\n ']}"
 # $(cat ${capacity}) -lt 33  && 
 if [ $(cat ${capacity}) -lt 33 ] && [ $(cat ${STATUSS}) == Discharging ]; then
-	su abdelilah -c "DISPLAY=':0' notify-send -t 60000  'The Power is Low Plug The Power Alimentation Now               hiiiii abdelilah' --icon=csd-power --app-name='csd power'"
+	su abdelilah -c "DISPLAY=':0' notify-send -t 60000  'The Power is Low Plug The Power Alimentation Now               hiiiii ' --icon=csd-power --app-name='csd power'"
 fi
 #grep Discharging $STATUSS
+# crontab edit 
+# m h  dom mon dow   command
+#*/2 * * * * /home/abdelilah/.local/bin/power.sh 2> /home/abdelilah/error-powery.txt
